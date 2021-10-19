14ed8a08659e00578b75dd690365572fb188f8f0
George Ndirangu <ndirangu.mepawa@gmail.com>
2021-10-19 16:17:16 +0300

    implement create brand,update brands migration

--- database/migrations/2021_10_12_103033_create_brands_table.php
+++ database/migrations/2021_10_12_103033_create_brands_table.php
@@ -15,7 +15,7 @@ class CreateBrandsTable extends Migration
     {
         Schema::create('brands', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 100);
+            $table->string('name', 100)->unique();
             $table->string('bio', 1000);
             $table->string('photoUrl');
             $table->timestamps();
